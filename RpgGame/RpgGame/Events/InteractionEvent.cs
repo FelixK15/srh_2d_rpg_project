f811aa5fd17ca905fd0b1e625dca33a382915999
Felix Klinge <f.klinge@k15games.de>
2013-01-31 00:17:52 +0100

    Forgot interactionevent

---
+++ RpgGame/RpgGame/Events/InteractionEvent.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Microsoft.Xna.Framework;
+
+namespace RpgGame.Events
+{
+    public class InteractionEvent : Event
+    {
+        public Rectangle InteractionArea { get; private set; }
+        public GameObject Source { get; private set; }
+
+        public InteractionEvent(Rectangle area,GameObject source)
+            : base(Event.Types.INTERACTION)
+        {
+            InteractionArea = area;
+            Source = source;
+        }
+    }
+}
