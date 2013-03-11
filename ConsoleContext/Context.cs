a585925f4028308f3552a92af633475344d636e5
Felix Klinge <f.klinge@k15games.de>
2013-03-11 15:52:50 +0100

    Added consolecontext project from which functions gets called via the developerconsole

---
+++ ConsoleContext/Context.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using RpgGame;
+using RpgGame.GameComponents;
+using RpgGame.Tools;
+
+namespace ConsoleContext
+{
+    public class Context
+    {
+        public void ShowComponents(string ObjectName)
+        {
+            GameObject Object = GameObject.AllObjects.Find(go => go.Name == ObjectName);
+            String Output = "";
+            if(Object != null){
+                foreach(IGameObjectComponent Component in Object.Components){
+                    Output += String.Format("{0} - {1}\n",Component.Name,Component.ToString());
+                }
+                DeveloperConsole.AddMessage(DeveloperConsole.MessageType.TRACE,Output);
+            }else{
+                DeveloperConsole.AddMessage(DeveloperConsole.MessageType.ERROR,"There's no object with the name '" + ObjectName + "'");
+            }
+        }
+    }
+}
