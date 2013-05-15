0a70d536a11920641b36977b5c2ad9ca9b5659e4
Felix Klinge <f.klinge@k15games.de>
2013-05-15 21:32:50 +0200

    FIXING TOBIAS STUFF!

---
+++ ConsoleContext/bin/Debug/GameScripts/Characters/Default.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+
+using Microsoft.Xna.Framework;
+using RpgGame;
+using RpgGame.GameComponents;
+
+public class Default
+{
+    public void Init()
+    {
+        //Parent will get add automaticly as a class member when used via script component
+        //..
+    }
+
+    public void Update(int deltaTime)
+    {
+        //Deltatime in milliseconds
+        //...
+    }
+
+    public void OnCollision(GameObject gameObject)
+    {
+        //gameobject = object that has been collided with
+        //..
+    }
+
+    public void OnTriggerEnter(GameObject gameObject)
+    {
+        //gameobject =  object that has been collided with
+        //..
+    }
+
+    public void OnTriggerLeave(GameObject gameObject)
+    {
+        //gameobject =  object that has been collided with
+        //..
+    }
+
+    public void OnInteraction(GameObject gameObject)
+    {
+        //gameobject = player
+        //..
+    }
+}
---
+++ RpgGame/RpgGame/bin/x86/Debug/Scripts/Enemies/Default.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace RpgGame.GameScripts.Enemies
+{
+    class Default
+    {
+        public void OnAttack(Weapon weapon,int damage)
+        {
+            //Attribute parent will be set automatically when used with script component
+            //weapon = weapon that hit the enemy
+            //damage = damage that the weapon did to that enemy (already calculated by the enemy component
+            //..
+        }
+
+        public void OnDeath(GameObject player)
+        {
+            //player = player that killed the enemy
+            //..
+        }
+    }
+}
